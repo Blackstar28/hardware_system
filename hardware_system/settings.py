7464f186276353b2f300c9ec304264fa3f4f6cf5
AJ <ajkleinf@gmail.com>
2025-04-12 23:47:55 +0800

    aded total sales in django admin

--- hardware_system/settings.py
+++ hardware_system/settings.py
@@ -63,7 +63,8 @@ ROOT_URLCONF = 'hardware_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
